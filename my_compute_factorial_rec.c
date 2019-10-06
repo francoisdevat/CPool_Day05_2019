afd715d7356418cc274d9ce2997e9569d7eb1741
françois de vathaire <fvathaire@localhost.localdomain>
2019-10-06 14:12:57 -0400

    add new my compute fac rec

--- my_compute_factorial_rec.c
+++ my_compute_factorial_rec.c
@@ -10,8 +10,9 @@ int my_compute_factorial_rec(int nb)
     if (nb == 1 || nb == 0) {
         return (1);
     }
-    else if (nb > 12 || n < 0) {
+    else if (nb > 12 || nb < 0) {
         return (0);
     }        
     return (nb * my_compute_factorial_rec(nb - 1));
 }
+
